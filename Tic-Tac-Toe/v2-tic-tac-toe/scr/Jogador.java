56120354e126c5ff62bd672f7013c6359ed704b0
Koehler <bernardoskoehler@gmail.com>
2026-04-10 09:08:32 -0300

    Update Jogador.java

--- Tic-Tac-Toe/v2-tic-tac-toe/scr/Jogador.java
+++ Tic-Tac-Toe/v2-tic-tac-toe/scr/Jogador.java
@@ -1,1 +1,26 @@
+public class Jogador {
+    private String nome;
+    private int simbolo;
 
+    Jogador(String nome, int simbolo) {
+        setNome(nome);
+        setSimbolo(simbolo);
+    }
+    private void setNome(String nome) {
+        if(nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
+        }
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    private void setSimbolo(int simbolo) {
+        if(simbolo == 1 || simbolo == 2) {
+            this.simbolo = simbolo;
+        }
+    }
+    public int getSimbolo() {
+        return simbolo;
+    }
+}
