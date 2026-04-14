dbca92db08c93033b30062f3013e9ea0fcd3eda9
Koehler <bernardoskoehler@gmail.com>
2026-04-14 00:13:18 -0300

    Update Itens.java

--- Top-Down/v1-top-down/scr/Itens.java
+++ Top-Down/v1-top-down/scr/Itens.java
@@ -1,1 +1,44 @@
+abstract class Itens {
+    Controle c = new Controle();
 
+    String nome;
+    int simbolo;
+
+    Itens(String nome, int simbolo) {
+        setNome(nome);
+        setSimbolo(simbolo);
+    }
+
+
+    protected void setNome(String nome) {
+        if(nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
+        }
+    }
+    protected void setSimbolo(int simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    protected void adicionarNoInventario(Personagem personagem) {
+        personagem.inventario.put(nome, this);
+    }
+    protected void removerDoInventario(Personagem personagem) {
+        if(!personagem.inventario.containsKey(nome)) {
+            System.out.println("Nao possui o item");
+            return;
+        }
+        personagem.inventario.remove(nome);
+    }
+    protected void apagarItemDoMapa(Mapa mapa) {
+        int linha = mapa.getLinha(simbolo);
+        int coluna = mapa.getColuna(simbolo);
+        mapa.mapa[linha][coluna] = 0;
+    }
+    protected void adicionarItemNoMapa(Mapa mapa, int linha, int coluna) {
+        if(!mapa.espacoDisponivel(linha, coluna)) {
+            System.out.println("Espaco ocupado!");
+            return;
+        }
+        mapa.mapa[linha][coluna] = simbolo;
+    }
+}
