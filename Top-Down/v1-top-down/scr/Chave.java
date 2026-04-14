1154c49256b20240511fba65892d4965ebff2252
Koehler <bernardoskoehler@gmail.com>
2026-04-14 00:12:43 -0300

    Update Chave.java

--- Top-Down/v1-top-down/scr/Chave.java
+++ Top-Down/v1-top-down/scr/Chave.java
@@ -1,1 +1,23 @@
+public class Chave extends Itens{
+    Chave() {
+        super("Chave", 2);
+    }
 
+    public void serPegoPorPersonagem(Mapa mapa, Personagem personagem) {
+        int linha = mapa.getLinha(simbolo);
+        int coluna = mapa.getColuna(simbolo);
+        if(mapa.mapa[linha][coluna] != 2) {
+            return;
+        }
+        if(mapa.mapa[linha + 1][coluna] == 1 || mapa.mapa[linha - 1][coluna] == 1 || mapa.mapa[linha][coluna + 1] == 1 || mapa.mapa[linha][coluna - 1] == 1) {
+            System.out.println("Pegue a chave: digite E");
+            String opcao = c.input();
+            if(opcao.equals("E") || opcao.equals("e")) {
+                adicionarNoInventario(personagem);
+                apagarItemDoMapa(mapa);
+                return;
+            }
+        }
+        return;
+    }
+}
