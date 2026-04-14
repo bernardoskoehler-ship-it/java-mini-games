f2983651b5203ad143072fa9fbfdd561b9a6f4de
Koehler <bernardoskoehler@gmail.com>
2026-04-14 00:12:29 -0300

    Update Porta.java

--- Top-Down/v1-top-down/scr/Porta.java
+++ Top-Down/v1-top-down/scr/Porta.java
@@ -1,1 +1,18 @@
+public class Porta extends Itens{
+    Porta() {
+        super("Porta", 4);
+    }
+    public void destrancar(Mapa mapa, Personagem personagem) {
+        int linha = mapa.getLinha(simbolo);
+        int coluna = mapa.getColuna(simbolo);
+        if(mapa.mapa[linha + 1][coluna] == 1 && personagem.inventario.containsKey("Chave")) {
+            System.out.println("Abra a porta: aperte E");
+            String opcao = c.input();
+            if(opcao.equals("E") || opcao.equals("e")) {
+                apagarItemDoMapa(mapa);
+                return;
+            }
+        }
+    }
 
+}
